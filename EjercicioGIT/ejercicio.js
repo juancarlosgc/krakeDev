a37f7ef7446364ef870d62dd0c72068a230f9cc7
juancarlosgc <juancarlos.gc@outlook.com>
2023-08-15 15:21:18 -0500

    Boton modificar imagen

--- EjercicioGIT/ejercicio.js
+++ EjercicioGIT/ejercicio.js
@@ -1,3 +1,7 @@
 saludar = function(){
+    let cmpImagenSaludo;
     console.log("Ingresa a Saludar");
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/gif2.gif";
+
 }
