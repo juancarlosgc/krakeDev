6f4f6cae302e6f85876434e8a33fe0954bce4baa
juancarlosgc <juancarlos.gc@outlook.com>
2023-08-16 16:28:00 -0500

    Cálculo de Promedio segunda parte OK

--- Condicionales/promedioNotas/promedioNotas.js
+++ Condicionales/promedioNotas/promedioNotas.js
@@ -1,11 +1,21 @@
 calcularPromedioNotas=function(){
-    let n1,n2,n3;
-    let cmpNota1,cmpNota2,cmpNota3;
+    let n1,n2,n3, resultado;
+    let cmpNota1,cmpNota2,cmpNota3,cmpImagen,cmpPromedio;
     cmpNota1=recuperarFlotante("txtNota1");
     n1 =parseFloat(cmpNota1);
     cmpNota2=recuperarFlotante("txtNota2");
     n2 =parseFloat(cmpNota2);
     cmpNota3=recuperarFlotante("txtNota3");
     n3 =parseFloat(cmpNota3);
-    calcularPromedio(n1,n2,n3);
+    resultado= calcularPromedio(n1,n2,n3);
+    cmpPromedio=document.getElementById("lblPromedio");
+    cmpPromedio.innerText=resultado;
+    if (resultado > 7){
+        cmpImagen=document.getElementById("imgPromedio");
+        cmpImagen.src="Exito.gif";
+    }else{
+        cmpImagen=document.getElementById("imgPromedio");
+        cmpImagen.src="Fracaso.gif";
+    }
+
 }
