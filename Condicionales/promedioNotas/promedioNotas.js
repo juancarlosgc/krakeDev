600c3437075a5ff2638285c9ac8e9089066bf46b
juancarlosgc <juancarlos.gc@outlook.com>
2023-08-19 11:39:53 -0500

    Se agrega operadores logicos a Reto Promedio Notas

--- Condicionales/promedioNotas/promedioNotas.js
+++ Condicionales/promedioNotas/promedioNotas.js
@@ -1,6 +1,6 @@
 calcularPromedioNotas=function(){
     let n1,n2,n3, resultado;
-    let cmpNota1,cmpNota2,cmpNota3,cmpImagen,cmpPromedio;
+    let cmpNota1,cmpNota2,cmpNota3,cmpImagen,cmpPromedio,cmpMensaje;
     cmpNota1=recuperarFlotante("txtNota1");
     n1 =parseFloat(cmpNota1);
     cmpNota2=recuperarFlotante("txtNota2");
@@ -10,12 +10,25 @@ calcularPromedioNotas=function(){
     resultado= calcularPromedio(n1,n2,n3);
     cmpPromedio=document.getElementById("lblPromedio");
     cmpPromedio.innerText=resultado;
-    if (resultado > 7){
-        cmpImagen=document.getElementById("imgPromedio");
-        cmpImagen.src="Exito.gif";
-    }else{
+    if (resultado > 0 && resultado < 5){
         cmpImagen=document.getElementById("imgPromedio");
         cmpImagen.src="Fracaso.gif";
+        cmpMensaje=document.getElementById("lblMensaje");
+        cmpMensaje.innerText=("Reprobado");
+    }else if (resultado >= 5 && resultado <= 8){
+        cmpImagen=document.getElementById("imgPromedio");
+        cmpImagen.src="Buen trabajo.gif";
+        cmpMensaje=document.getElementById("lblMensaje");
+        cmpMensaje.innerText=("BUEN TRABAJO");
+    }else if (resultado > 8 && resultado <= 10){
+        cmpImagen=document.getElementById("imgPromedio");
+        cmpImagen.src="excelente.gif";
+        cmpMensaje=document.getElementById("lblMensaje");
+        cmpMensaje.innerText=("EXCELENTE");
+    }else{
+        cmpImagen=document.getElementById("imgPromedio");
+        cmpImagen.src="error.gif";
+        cmpMensaje=document.getElementById("lblMensaje");
+        cmpMensaje.innerText=("DATOS INCORRECTOS");
     }
-
 }
