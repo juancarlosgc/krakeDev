0ac72a83fde80d4111cbcdff9b00f9ec0dd358c9
juancarlosgc <juancarlos.gc@outlook.com>
2023-08-24 18:43:23 -0500

    Ejercicio For Reto 37

---
+++ EjerciciosFor/ejemplosFor.js
@@ -0,0 +1,46 @@
+mostrarNumeros1 = function(){
+    console.log("Antes de for");
+    for (let i=0;i<4;i++){
+        console.log(i);
+    }
+    console.log("Despues de for");
+}
+
+mostrarNumeros2=function(){
+    console.log("Antes de for");
+    for (let indice=1; indice<5;indice++ ){
+        console.log(indice);
+    }
+    console.log("Despues de for");
+}
+
+mostrarPares=function(){
+    console.log("Antes de for");
+    for (let x=2; x<=10; x+=2 ){
+        console.log(x);
+    }
+    console.log("Despues de for");
+}
+
+mostrarInverso=function(){
+    console.log("Antes de for");
+    for (let i=10; i>=0; i-- ){
+        console.log(i);
+    }
+    console.log("Despues de for");
+}
+
+hackearNasa=function(){
+    console.log("Antes de for");
+    for (let porcentaje=0; porcentaje<=100; porcentaje+=10 ){
+        console.log("Hackeando NASA " + porcentaje + " %"  );
+    }
+    console.log("Despues de for");
+}
+
+mostrarImpares=function(){
+    for(i=1;i<=21;i+=2){
+        console.log(i);
+    }
+}
+
